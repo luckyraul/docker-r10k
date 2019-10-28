9b97f7b46e174dbeae7054cb69b8ff522c95db17
nikita tarasov <nikita@mygento.ru>
2019-10-28 15:31:53 +0300

    split to a new repo

---
+++ Puppetfile
@@ -0,0 +1,8 @@
+forge "https://forgeapi.puppetlabs.com"
+
+mod 'puppetlabs/dummy_service'
+mod 'puppet-r10k', '8.0.0'
+mod 'puppetlabs/stdlib'
+mod 'puppetlabs/git'
+mod 'puppetlabs/vcsrepo'
+mod 'puppetlabs/inifile'
---
+++ manifests/init.pp
@@ -0,0 +1,38 @@
+lookup('classes', {merge => unique}).include
+include 'dummy_service'
+
+ensure_packages(['openssh-client'], {'ensure' => 'present'})
+
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+
+exec { 'ssh_known_host_bitbucket':
+  command => 'ssh-keyscan -t rsa bitbucket.org >> /etc/ssh/ssh_known_hosts',
+  unless  => 'grep bitbucket.org /etc/ssh/ssh_known_hosts',
+  user    => 'root',
+  require => Package['openssh-client'],
+} -> class { 'r10k':
+  sources => {
+    'puppet' => {
+      'remote'  => 'git@bitbucket.org:mygento/puppet-server.git',
+      'basedir' => '/etc/puppetlabs/code/environments',
+      'prefix'  => false,
+    }
+  },
+} -> file {'/etc/r10k.yaml':
+  ensure => 'link',
+  target => '/etc/puppetlabs/r10k/r10k.yaml',
+}
+
+class {'r10k::webhook::config':
+  use_mcollective => false,
+  enable_ssl      => false,
+  protected       => false,
+}
+
+class {'r10k::webhook':
+  use_mcollective => false,
+  user            => 'root',
+  group           => 'root',
+  background      => false,
+  require         => Class['r10k::webhook::config'],
+}
